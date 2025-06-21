464ebf56586b245894a367bd45672060af0b2c99
dmoss1400 <douglasmoss1400@gmail.com>
2025-06-21 11:21:50 -0500

    Add files via upload

---
+++ Vendidit-Calculator/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "20px" }}>
+      <h1>Vendidit Calculator</h1>
+      <p>This is the Vendidit Calculator React App placeholder.</p>
+    </div>
+  );
+}
